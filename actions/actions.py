3fb098e27e439da322ad97f46ae6448ae049147e
Paloma Piot <paloma.piot@udc.es>
2021-04-18 22:39:22 +0200

    Find movies in trakt

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,46 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+ 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
-# This is a simple example for a custom action which utters "Hello World!"
+import json
+from urllib.request import Request, urlopen
+ 
+class ActionValidateMovieForm(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "validate_movie_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        movie_name = tracker.get_slot('movie')
+        movie_name = movie_name.replace(' ', '%20')
+
+        # Auth trakt
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        request = Request('https://api.trakt.tv/oauth/authorize', headers=headers)
+        response_body = urlopen(request).read()
+
+        # Search movie
+        headers = {
+            'Content-Type': 'application/json',
+            'trakt-api-version': '2',
+            'trakt-api-key': '[client_id]' # client_id
+        }
+        request = Request('https://api.trakt.tv/search/movie?query=' + movie_name, headers=headers)
+        response_body = urlopen(request)
+        movies = json.load(response_body)
+        response_movies = [movie['movie']['title'] for movie in movies]
+
+        result_text = "Found the movies: " + str(response_movies)
+        dispatcher.utter_message(text=result_text)
+
+        return [SlotSet("movie", response_movies if response_movies is not None else [])]
