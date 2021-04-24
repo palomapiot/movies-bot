f139013612995a4dc1dd4ec0dc5e4d69b0561b8a
Paloma Piot <paloma.piot@udc.es>
2021-04-24 17:05:58 +0200

    update actions

--- actions/actions.py
+++ actions/actions.py
@@ -14,9 +14,10 @@ import requests
 import json
 from urllib.request import Request, urlopen
 
-CLIENT_ID = '99c154b68b67951f7a21354baa5f0afb68a9207e4c60f96bf08af0500f4602c5'
-CLIENT_SECRET = 'xxxxx'
 BASE_URL = 'https://api.trakt.tv'
+TRAKT_URL = 'https://trakt.tv'
+
+
 
 class ActionValidateMovieForm(Action):
 
@@ -69,9 +70,6 @@ class ActionValidateDownloadMovieForm(Action):
 
         movie_id = tracker.get_slot('selected_movie')
         if (movie_id is not None):
-            # Get bearer token
-            #bearer_token = token()
-
             # Get movie by ID
             headers = {
                 'Content-Type': 'application/json',
@@ -82,20 +80,12 @@ class ActionValidateDownloadMovieForm(Action):
             movie_body = urlopen(movie_request)
             movie_result = json.load(movie_body)
             movie = [m['movie'] for m in movie_result]
-
-
-            """# Add movie to Watchlist           
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + bearer_token,
-                'trakt-api-version': '2',
-                'trakt-api-key': CLIENT_ID
-            }
-            movies_body = {
-                'movies': movie
-            }
-            watchlist_request = requests.post(BASE_URL + '/sync/watchlist', data=movies_body, headers=headers)"""
-            # Send movie to a chat
+            print(movie_result)
+            movie_slug = movie_result[0]['movie']['ids']['slug']
+            print(movie_slug)
+            
+            # Send movie to a telegram chat
+            telegram_bot_sendtext("Please download the movie: " + TRAKT_URL + '/movies/' + movie_slug)
 
             dispatcher.utter_message(text="Starting to download your movie! Will be available in Diegoflix soon.")
         else:
@@ -110,35 +100,9 @@ def auth():
     request = Request(BASE_URL + '/oauth/authorize', headers=headers)
     response_body = urlopen(request).read()
 
-"""def code():
-    values =  {
-        'client_id': CLIENT_ID
-    }
+def telegram_bot_sendtext(bot_message):
+   send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ID + '&parse_mode=Markdown&text=' + bot_message
 
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(BASE_URL + '/oauth/device/code', json=values, headers=headers).json()
-    print(response)
-    
-    # todo: go to the verification url and enter user code
+   response = requests.get(send_text)
 
-    return response.get('device_code')
-
-def token():
-    auth()          
-    device_code = code()
-
-    values = {
-        "code": device_code,
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET
-    }
-    print(values)
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(BASE_URL + '/oauth/device/token', json=values, headers=headers)#.json()
-    print(response)
-    data = response.json()
-    return data.get('access_token')"""
+   return response.json()
